ff6b38b4dfad6753283305d7ea9da9ef66706ccd
Nils Werner <nils.werner@gmail.com>
2009-08-08 14:31:08 +0200

    Initial Release

---
+++ README.txt
@@ -0,0 +1,29 @@
+
+Admin Rainbow Headline
+------------------------------------
+
+Version: 1.0
+Author: Nils Werner (nils.werner@gmail.com)
+Build Date: 2009-08-08
+Requirements: Symphony 2.0.6
+
+[ABOUT]
+
+Have you ever had two or more installations of Symphony open in several tabs? Have you ever had problems differentiating 
+those tabs, making changes to one of those sites that were supposed to go into another one?
+
+Admin Rainbow Headline comes to the rescue. It lets you set any RGB-Color for the headline (the dark grey block at the top
+of Symphonys backend), making it easier to differentiate them.
+
+[INSTALLATION]
+
+1. Upload the 'admin_rainbow_headline' folder in this archive to your Symphony 'extensions' folder.
+
+2. Enable it by selecting the "Admin Rainbow Headline" entry, choose Enable from the with-selected menu, then click Apply.
+
+3. You can now set the headline color in your preferences.
+
+[CHANGELOG]
+
+1.0
+- Initial release
---
+++ extension.driver.php
@@ -0,0 +1,80 @@
+<?php
+
+	Class extension_admin_rainbow_headline extends Extension{
+	
+		public function about(){
+			return array('name' => 'Admin Rainbow Headline',
+						 'version' => '1.0',
+						 'release-date' => '2009-08-08',
+						 'author' => array('name' => 'Nils Werner',
+										   'website' => 'http://www.phoque.com/projekte/symphony',
+										   'email' => 'nils.werner@gmail.com')
+				 		);
+		}
+		
+		
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page'		=> '/backend/',
+					'delegate'	=> 'InitaliseAdminPageHead',
+					'callback'	=> 'initaliseAdminPageHead'
+				),
+						
+				array(
+					'page' => '/system/preferences/',
+					'delegate' => 'AddCustomPreferenceFieldsets',
+					'callback' => 'appendPreferences'
+				),	
+				
+				array(
+
+					'page' => '/system/preferences/',
+					'delegate' => 'Save',
+					'callback' => 'savePreferences'
+
+				),
+			);
+		}
+
+		public function initaliseAdminPageHead($context) {
+			$page = $context['parent']->Page;
+			
+			if(General::Sanitize(Administration::instance()->Configuration->get('headline_color', 'admin_rainbow_headline')) != "") {
+				$page->addElementToHead(new XMLElement("style", "
+				body form h1 { background-color: " . General::Sanitize(Administration::instance()->Configuration->get('headline_color', 'admin_rainbow_headline')) . "; }
+			", array("type" => "text/css", "media" => "screen, projection")), 100012);
+			}
+		}
+		
+		public function savePreferences($context){
+			
+			$headline_color = trim(strtoupper($context['settings']['admin_rainbow_headline']['headline_color']));
+			if(strlen($headline_color) > 0 and substr($headline_color,0,1) != "#") {
+				$headline_color = "#" . $headline_color;
+			}
+			
+			$context['settings']['admin_rainbow_headline'] = array("headline_color" => substr(trim($headline_color), 0, 7));
+
+
+		}
+
+		public function appendPreferences($context){
+			
+			$group = new XMLElement('fieldset');
+			$group->setAttribute('class', 'settings');
+			$group->appendChild(new XMLElement('legend', 'Admin Rainbow Headline'));
+
+
+			$label = Widget::Label('Headline Background Color');
+			$label->appendChild(Widget::Input('settings[admin_rainbow_headline][headline_color]', General::Sanitize(Administration::instance()->Configuration->get('headline_color', 'admin_rainbow_headline'))));		
+			$group->appendChild($label);
+			$group->appendChild(new XMLElement('p', 'This can be any RGB-Hexvalue, for example <code>#97712B</code>', array('class' => 'help')));
+						
+			$context['wrapper']->appendChild($group);
+						
+		}
+			
+	}
+
+?>
