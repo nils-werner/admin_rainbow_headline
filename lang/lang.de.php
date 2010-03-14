9cbd840c56e1b5e2e7211a699c05bf85f9e1a9f5
Nils Werner <nils.werner@gmail.com>
2010-03-14 12:12:49 +0100

    German Translation

---
+++ lang/lang.de.php
@@ -0,0 +1,31 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Nils Werner',
+			'email' => 'nils.werner@gmail.com',
+			'website' => 'http://www.phoque.de'
+		),
+		'release-date' => '2010-03-14'
+	);
+	
+	
+	/*
+	 * EXTENSION: Export Ensemble
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+
+		'Admin Rainbow Headline' =>
+		'Regenbogen-Kopfzeile',
+
+		'Headline Background Color' =>
+		'Kopfzeilen-Hintergrundfarbe',
+
+		'This can be any RGB-Hexvalue, for example <code>#97712B</code>' =>
+		'Jeder RGB-Hexwert ist möglich, z.B. <code>#97712B</code>',
+
+	);
+	
